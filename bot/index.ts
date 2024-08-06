4995904e6a39a01afded5a72e75be88edb22e3e1
shahbaz4783 <shahbaz4783@gmail.com>
2024-08-06 11:59:30 +0530

    bot code

--- bot/index.ts
+++ bot/index.ts
@@ -1,13 +1,25 @@
+import 'dotenv/config';
 import { Telegraf } from 'telegraf';
 
-const bot = new Telegraf(process.env.BOT_TOKEN || '');
+const BOT_TOKEN = process.env.BOT_TOKEN;
+
+if (!BOT_TOKEN) {
+	throw new Error('BOT_TOKEN is not set in the environment variables.');
+}
+
+const bot = new Telegraf(BOT_TOKEN);
 
 bot.start((ctx) => {
 	const startPayload = ctx.startPayload;
-	const user = ctx.message.from;
+	const user = ctx.message?.from;
+	if (!user) {
+		ctx.reply('Could not get user information');
+		return;
+	}
+
 	const userName = user.username ? `@${user.username}` : user.first_name;
 
-	ctx.replyWithMarkdown(`Hey ${userName} welcome to test app`, {
+	ctx.replyWithMarkdown(`Hey ${userName}, welcome to test app`, {
 		reply_markup: {
 			inline_keyboard: [
 				[{ text: 'Start Now', web_app: { url: 'https://google.com' } }],
