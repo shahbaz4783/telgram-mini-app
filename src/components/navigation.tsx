afa252de8d01142562cb7dd9b10cb4e5a457d3aa
shahbaz4783 <shahbaz4783@gmail.com>
2024-08-07 01:03:39 +0530

    fix missing key

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -5,7 +5,7 @@ const Navigation = () => {
 	return (
 		<nav className='sticky bottom-2 m-auto flex justify-evenly py-2 gap-6 w-full border-t-2 backdrop-blur'>
 			{NAVIGATION_LINKS.map((item) => (
-				<Link href={item.href}>
+				<Link key={item.href} href={item.href}>
 					<div className='flex flex-col justify-center gap-1 items-center'>
 						{item.icon && <item.icon />}
 						<p>{item.title}</p>
